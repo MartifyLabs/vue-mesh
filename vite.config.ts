28121d240a6a58bba5b757b61fd92e4c075f8a7b
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-12-04 14:10:00 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      util: 'util',
+    },
+  },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
+});
